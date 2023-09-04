e72b3eb1b333f1c6b652d08d6ccc64ab389d0fa3
Aditya <adityasaikumar.bodapati@gmail.com>
2023-09-04 22:45:45 +0530

    Docker containerzation commit

--- src/main/java/com/example/demo/SecondProjectApplication.java
+++ src/main/java/com/example/demo/SecondProjectApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecondProjectApplication {
+@RestController	
 
+public class SecondProjectApplication {
+	
+	@GetMapping("/home")
+	public String getMessage() {
+		return "Hello to the Container World of Docker!...ADITYA" ;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SecondProjectApplication.class, args);
 	}
-
 }
